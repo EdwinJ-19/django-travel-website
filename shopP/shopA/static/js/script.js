c7d3f996b35d3564973a55ac557f3fc9b06b3cb4
EdwinJ-19 <edwinjames2001@gmail.com>
2024-01-15 16:29:09 +0530

    about_me_updated

--- shopP/shopA/static/js/script.js
+++ shopP/shopA/static/js/script.js
@@ -11,3 +11,20 @@ function btn_2(){
 function btn_3(){
     window.location.href='register';
 }
+
+// const typing = document.querySelector('.typing .text.sec-text');
+
+// const textLoad = () => {
+//     setTimeout(()=>{
+//         text.textContent = "Front-End Developer";
+//     },0);
+//     setTimeout(()=>{
+//         text.textContent = "Photographer";
+//     },2000);
+//     setTimeout(()=>{
+//         text.textContent = "Gamer";
+//     },4000);    
+//     setTimeout(()=>{
+//         text.textContent = "Team-Player";
+//     },6000);
+// }
